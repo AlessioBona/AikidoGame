d68f39dc4aa32dd19d091aacd560f43f83b82121
Ara Hacopian <arahacopian@gmail.com>
2018-11-17 23:17:50 +0100

    Add enemy spawn and AI behavior

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public int maxEnemies;
+    public int count = 0;
+    public float repeatTime = 5f;
+    public float startTime = 2f;
+    public GameObject prefab;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", startTime, repeatTime);
+    }
+
+    void Spawn()
+    {
+        if (count >= maxEnemies) return;
+        count++;
+        Instantiate(GetPrefab(), transform.position, Quaternion.identity);
+    }
+
+    private GameObject GetPrefab()
+    {
+        return prefab;
+    }
+}
